436c94bd5f7aa02d1cd0e4155168d1ecd4249918
Jun Kang <jun@ip-172-31-1-179.ap-northeast-2.compute.internal>
2016-08-31 05:07:58 +0000

    consensus clusterinf n=5

--- pancancer_immune.R
+++ pancancer_immune.R
@@ -7,7 +7,7 @@ rdate <- getFirehoseRunningDates(last = NULL)
 
 dset <- getFirehoseDatasets()
 dset <- dset[c(2,3,6,12,13,21,22,24,30)]#,31,34)]
-
+dset <- c('BRCA', 'LUAD')
 gs <- gs_gmt('custom.bindea.gmt')
 
 data <- pancancer_ssGSEA(dset)
@@ -19,10 +19,39 @@ TvsN <- factor(TvsN, levels = c('T', 'N'),
                labels = c('Tumor', 'Normal'))
 summary(TvsN)
 
-ann <- data.frame(type = data[[2]], Tumor = TvsN)
+ann <- data.frame(class = as.factor(results_row[[6]]$consensusClass),
+                  type = data[[2]], 
+                  Tumor = TvsN)
+
+library(ConsensusClusterPlus)
+
+results_col = ConsensusClusterPlus(data_t,maxK=5,reps=5000,pItem=0.8,pFeature=1,
+                                   title='consensus_col',
+                                   clusterAlg="hc",
+                                   innerLinkage = "ward.D2",
+                                   finalLinkage = "ward.D2",
+                                   distance="euclidean",
+                                   plot="pdf")
+
+results_row = ConsensusClusterPlus(data[[1]],maxK=10,reps=500,pItem=0.8,pFeature=1,
+                                   title='consensus_by_row',
+                                   clusterAlg="hc",
+                                   innerLinkage = "ward.D2",
+                                   finalLinkage = "ward.D2",
+                                   distance="euclidean",
+                                   plot="pdf")
 
 library(NMF)
+library(Cairo)
+ann_col <- data.frame(immune_class = factor(results_col[[3]]$consensusClass))
+
+CairoPDF(file = 'pancancer_TCGA.pdf',
+         width =7.5, height = 7.5, pointsize = 16)
 aheatmap(data_t,
          hclustfun=function(d) hclust(d, method="ward.D2"),
-         annRow = ann)
-
+         annRow = ann,
+         annCol = ann_col,
+         Colv = results_col[[3]]$consensusTree,
+         Rowv = results_row[[6]]$consensusTree,
+         labRow = rep('',dim(data_t)[1]))
+dev.off()
